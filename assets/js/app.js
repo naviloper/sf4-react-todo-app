b83ba0119df3a5859eafcc833b8abf73235d0c49
navid045 <navid045@gmail.com>
2019-06-07 23:46:37 +0430

    Console log commented

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,4 +11,4 @@ require('../css/app.css');
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // const $ = require('jquery');
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+//console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
