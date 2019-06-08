8c6ef31acfcf31409f3d1079f34a9fee392d4a55
navid045 <navid045@gmail.com>
2019-06-08 22:39:37 +0430

    Changes in webpack configs to work with react

--- webpack.config.js
+++ webpack.config.js
@@ -23,7 +23,7 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
      */
-    .addEntry('app', './assets/js/app.js')
+    .addEntry('app', './frontapp/src/index.js')
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -69,6 +69,12 @@ Encore
     // uncomment if you use API Platform Admin (composer req api-admin)
     //.enableReactPreset()
     //.addEntry('admin', './assets/js/admin.js')
+
+    .enableReactPreset()
+
+    .configureBabel(function(babelConfig) {
+        babelConfig.plugins.push('@babel/plugin-proposal-class-properties');
+    })
 ;
 
 module.exports = Encore.getWebpackConfig();
